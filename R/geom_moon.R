6f7f12ba8eabd9c82df4098dd7481429086e4261
Michael Bramson <mnbramson@gmail.com>
2019-10-21 01:08:38 +0000

    Call functions from imported packages explicitly with ::

--- R/geom_moon.R
+++ R/geom_moon.R
@@ -1,27 +1,26 @@
-library(ggplot2)
-
-GeomMoon <- ggproto(
-  "GeomMoon", Geom,
+GeomMoon <- ggplot2::ggproto(
+  "GeomMoon", ggplot2::Geom,
   
   required_aes = c("x", "y"),
   
-  default_aes = aes(
+  default_aes = ggplot2::aes(
     ratio = 0.25, right = TRUE, size = 3, angle = 0,
     colour = "black", fill = "white", alpha = NA,
     stroke = 0.25, linetype = 1
   ),
   
-  draw_key = draw_key_point,
+  # TODO: Make a proper legend key
+  draw_key = ggplot2::draw_key_point,
   
   draw_panel = function(data, panel_params, coord) {
     coords <- coord$transform(data, panel_params)
     moonGrob(
       coords$x, coords$y, ratio = coords$ratio, right = coords$right,
       size = coords$size, angle = coords$angle,
-      gp = gpar(
-        col = alpha(coords$colour, coords$alpha),
-        fill = alpha(coords$fill, coords$alpha),
-        lwd = coords$stroke * .stroke,
+      gp = grid::gpar(
+        col = scales::alpha(coords$colour, coords$alpha),
+        fill = scales::alpha(coords$fill, coords$alpha),
+        lwd = coords$stroke * ggplot2::.stroke,
         lty = coords$linetype
       )
     )
@@ -33,7 +32,7 @@ geom_moon <- function(
   mapping = NULL, data = NULL, stat = "identity", position = "identity",
   na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...
 ) {
-  layer(
+  ggplot2::layer(
     geom = GeomMoon, mapping = mapping, data = data, stat = stat, 
     position = position, show.legend = show.legend,
     inherit.aes = inherit.aes, params = list(na.rm = na.rm, ...)
@@ -44,14 +43,13 @@ geom_moon <- function(
 
 # Examples ----------------------------------------------------------------
 
-# df <- tibble(
+# df <- data.frame(
 #   x = c(1:5, NA), y = 1:6, gibbosity = 0:5 / 5, size = 2:7
 # )
 # 
-# ggplot(df, aes(x, y, size = size)) +
-#   geom_moon(aes(ratio = gibbosity), fill = "red", color = "red") +
+# ggplot2::ggplot(df, ggplot2::aes(x, y, size = size)) +
+#   geom_moon(ggplot2::aes(ratio = gibbosity), fill = "red", color = "red") +
 #   geom_moon(
-#     aes(ratio = 1 - gibbosity),
+#     ggplot2::aes(ratio = 1 - gibbosity),
 #     fill = "blue", color = "blue", right = FALSE
 #   )
-
--- R/moonGrob.R
+++ R/moonGrob.R
@@ -1,5 +1,3 @@
-library(grid)
-
 moonGrob <- function(
   x, y, ratio = 0.25, right = TRUE, size = 1, angle = 0,
   default.units = "npc", size.units = "mm", ...
@@ -44,10 +42,10 @@ moonGrob <- function(
     )
   )
   
-  x_coords <- do.call(unit.c, coords_list["x_coords",])
-  y_coords <- do.call(unit.c, coords_list["y_coords",])
+  x_coords <- do.call(grid::unit.c, coords_list["x_coords",])
+  y_coords <- do.call(grid::unit.c, coords_list["y_coords",])
   
-  polygonGrob(
+  grid::polygonGrob(
     x_coords, y_coords,
     id.lengths = sapply(coords_list["x_coords",], length),
     default.units = default.units, ...
@@ -59,7 +57,7 @@ moonGrob <- function(
 grid.moon <- function(..., draw = TRUE) {
   mg <- moonGrob(...)
   if (draw) {
-    grid.draw(mg)
+    grid::grid.draw(mg)
   }
   invisible(mg)
 }
@@ -68,6 +66,12 @@ grid.moon <- function(..., draw = TRUE) {
 moon_coords <- function(
   x, y, ratio, right, size, angle, default.units, size.units
 ) {
+  # The circles and ellipses in gggibbous are actually grid's xspline
+  # approximation of a cubic B-spline approximation of a Bezier
+  # approximation of an ellipse. As such, the "magic" number used here
+  # to determine the control points is slightly different than what one would
+  # use for a true Bezier curve. This magic number has been determined
+  # empirically by eye; it could probably be further refined.
   magic <- 0.571 # Magic number for Bezier approximation of ellipse
   
   # Calculate points of Bezier approximations of circle and ellipse
@@ -75,12 +79,12 @@ moon_coords <- function(
   # First center the circle and ellipse at the origin (using size.units),
   # then we will translate everything to the right location (using
   # default.units).
-  bez_circ_top <- bezierPoints(bezierGrob(
+  bez_circ_top <- grid::bezierPoints(grid::bezierGrob(
     c(0, magic * size, size, size),
     c(size, size, magic * size, 0),
     default.units = size.units
   ))
-  bez_circ_bot <- bezierPoints(bezierGrob(
+  bez_circ_bot <- grid::bezierPoints(grid::bezierGrob(
     c(size, size, magic * size, 0),
     c(0, -magic * size, -size, -size),
     default.units = size.units
@@ -91,13 +95,13 @@ moon_coords <- function(
   # Crescent or gibbous?
   x_dir <- ifelse(ratio > 0.5, 1, -1)
   
-  bez_elli_bot <- bezierPoints(bezierGrob(
+  bez_elli_bot <- grid::bezierPoints(grid::bezierGrob(
     c(0, x_dir * magic * size * e_scale,
       x_dir * size * e_scale, x_dir * size * e_scale),
     c(-size, -size, -magic * size, 0),
     default.units = size.units
   ))
-  bez_elli_top <- bezierPoints(bezierGrob(
+  bez_elli_top <- grid::bezierPoints(grid::bezierGrob(
     c(x_dir * size * e_scale, x_dir * size * e_scale,
       x_dir * magic * size * e_scale, 0),
     c(0, magic * size, size, size),
@@ -105,13 +109,13 @@ moon_coords <- function(
   ))
   
   # Get x and y coordinates for whole perimeter
-  poly_x <- unit.c(
+  poly_x <- grid::unit.c(
     bez_circ_top$x,
     bez_circ_bot$x[2:length(bez_circ_bot$x)],
     bez_elli_bot$x[2:length(bez_elli_bot$x)],
     bez_elli_top$x[2:(length(bez_elli_top$x) - 1)]
   )
-  poly_y <- unit.c(
+  poly_y <- grid::unit.c(
     bez_circ_top$y,
     bez_circ_bot$y[2:length(bez_circ_bot$y)],
     bez_elli_bot$y[2:length(bez_elli_bot$y)],
@@ -120,15 +124,15 @@ moon_coords <- function(
   
   # For a left-side moon, flip the x values around the origin
   if(right == FALSE) {
-    poly_x <- unit(0, size.units) - poly_x
+    poly_x <- grid::unit(0, size.units) - poly_x
   }
   
   # Translate the shape to the specified center point
-  if (!is.unit(x)) {
-    x <- unit(x, default.units)
+  if (!grid::is.unit(x)) {
+    x <- grid::unit(x, default.units)
   }
-  if (!is.unit(y)) {
-    y <- unit(y, default.units)
+  if (!grid::is.unit(y)) {
+    y <- grid::unit(y, default.units)
   }
   trans_x <- poly_x + x
   trans_y <- poly_y + y
@@ -185,10 +189,10 @@ moon_coords <- function(
 #   0.8, 0.5, ratio = 0.25, size = 10, default.units = "npc"
 # )
 
-# grid.newpage()
+# grid::grid.newpage()
 # grid.moon(
 #   x = rep(1:10/11, 10), y = rep(10:1/11, each = 10), ratio = 1:100/100,
-#   size = 3, gp = gpar(
+#   size = 3, gp = grid::gpar(
 #     col = hcl(0:99*3.6, c = 60, l = 70),
 #     fill = hcl(0:99*3.6, c = 60, l = 70)
 #   )
