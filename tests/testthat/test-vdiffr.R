6befa5e4eab4068b37b08823eb99b67bd9660cc7
Michael Bramson <mnbramson@gmail.com>
2019-10-27 03:28:31 +0000

    Updated vdiffr tests

--- tests/testthat/test-vdiffr.R
+++ tests/testthat/test-vdiffr.R
@@ -1,18 +1,27 @@
-five_moons <- ggplot(
-  data.frame(x = 1:5, y = 1, size = 1:5, ratio = 1:5 * 0.2),
-  aes(x = x, y = y, size = size, ratio = ratio)
-) +
-  geom_moon() +
-  scale_size_continuous(range = c(1, 20))
+five_moons <- data.frame(
+  x = 1:5,
+  y = 1,
+  size = 2^(1:5),
+  ratio = 0:4 * 0.25,
+  group = letters[1:5]
+)
 
-vdiffr::expect_doppelganger("five moons", five_moons)
+moon_size <- ggplot(five_moons, aes(x = x, y = y, size = size)) +
+  geom_moon(ratio = 0.5, right = FALSE) +
+  scale_size_area(max_size = 10, guide = "none")
 
-moon_pseudomap <- ggplot(
-  subset(dmeladh, N > 200),
-  aes(Longitude, Latitude)
-) +
-  geom_moon(aes(ratio = AdhF / 100), fill = "black") +
-  geom_moon(aes(ratio = AdhS / 100), right = FALSE) +
-  coord_fixed()
+vdiffr::expect_doppelganger("moons scale correctly", moon_size)
 
-vdiffr::expect_doppelganger("moon pseudo-map", moon_pseudomap)
+moon_missing <- ggplot(
+  five_moons, aes(x = x, y = y, ratio = ratio, fill = group, color = group)) +
+  geom_moon(size = 5) +
+  scale_color_discrete(guide = guide_legend(
+    keywidth = unit(13, "mm"), keyheight = unit(13, "mm")))
+
+vdiffr::expect_doppelganger("zero ratio moons are invisible", moon_missing)
+
+full_moons <- ggplot(five_moons, aes(x = x, y = y)) +
+  geom_moon(aes(ratio = ratio), fill = "black", size = 5) +
+  geom_moon(aes(ratio = 1 - ratio), right = FALSE, size = 5)
+
+vdiffr::expect_doppelganger("moons can be complemented", full_moons)
