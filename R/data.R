24941e92edbb8038db413a5c6e3a38f0d435c8ce
Michael Bramson <mnbramson@gmail.com>
2019-10-30 01:12:39 +0000

    Added lunar distance example to vignette and added accompanying sample data

--- R/data.R
+++ R/data.R
@@ -14,6 +14,23 @@
 #'   \item{Longitude}{longitude of population sample}
 #'   \item{N}{number of samples}
 #'   \item{AdhF}{percent of samples with \emph{Adh^F} allele, as an integer}
-#'   \item{AdhF}{percent of samples with \emph{Adh^S} allele, as an integer}
+#'   \item{AdhS}{percent of samples with \emph{Adh^S} allele, as an integer}
 #' }
 "dmeladh"
+
+#' Lunar distances and principal phases for 2019
+#'
+#' This data set contains the distance from the Earth to the Moon for each day
+#' in 2019, as well as the dates (in UTC) of each occurrence of the four
+#' principal phases of the moon. The data are adapted from NASA.
+#' 
+#' @format A data frame with 365 rows and 3 variables:
+#' \describe{
+#'   \item{date}{Date}
+#'   \item{distance}{Distance from the Earth to the Moon in kilometers}
+#'   \item{phase}{Principal phase of the moon (full, new, first quarter, third
+#'   quarter) or NA if no principal phase on that date}
+#' }
+#' 
+#' @source \url{https://svs.gsfc.nasa.gov/4442}
+"lunardist"
