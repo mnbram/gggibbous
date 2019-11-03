4b0ddbe50f159590d7f3f3e6722caf4a7dcf45e6
Michael Bramson <mnbramson@gmail.com>
2019-11-03 14:08:16 +0000

    Added draw_key_full_moon

--- R/geom_moon.R
+++ R/geom_moon.R
@@ -4,7 +4,9 @@
 #'
 #' \code{draw_key_moon} (the default in \code{geom_moon}) draws a gibbous moon
 #' filled from the right. \code{draw_key_moon_left} draws a crescent moon from
-#' the right.
+#' the right. \code{draw_key_full_moon} draws a circle, which is very similar to
+#' \code{draw_key_point} in \code{ggplot2}, but the size is calculated slightly
+#' differently and the default aesthetics differ.
 #'
 #' @inheritParams ggplot2::draw_key
 #' @export
@@ -42,6 +44,25 @@ draw_key_moon_left <- function(data, params, size) {
       lty = d_ltype
     ),
     right = FALSE
+  )
+}
+
+#' @rdname draw_key_moon
+#' @export
+draw_key_full_moon <- function(data, params, size) {
+  d_size <- ifelse(is.null(data$size), 10, data$size)
+  d_col <- ifelse(is.null(data$colour), "black", data$colour)
+  d_fill <- ifelse(is.null(data$fill), "white", data$fill)
+  d_stroke <- ifelse(is.null(data$stroke), 0.25, data$stroke)
+  d_ltype <- ifelse(is.null(data$linetype), "solid", data$linetype)
+  moonGrob(
+    0.5, 0.5, r = d_size / 2 * 0.75, ratio = 1,
+    gp = grid::gpar(
+      col = scales::alpha(d_col, data$alpha),
+      fill = scales::alpha(d_fill, data$alpha),
+      lwd = d_stroke * .stroke,
+      lty = d_ltype
+    )
   )
 }
 
