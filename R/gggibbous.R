ac44e570e8b745f3b63ae903c32d536c1eab4326
Michael Bramson <mnbramson@gmail.com>
2019-11-03 14:19:30 +0000

    Updated package help file

--- R/gggibbous.R
+++ R/gggibbous.R
@@ -1,17 +1,20 @@
-#' gggibbous: Moon charts, a pie chart alternative
-#' 
-#' Moon charts are like pie charts except that the proportions are
-#' shown as crescent or gibbous portions of a circle, like the lit and unlit
-#' portions of the moon. As such, they work best with only one or two groups.
-#' gggibbous extends ggplot2 to allow for plotting multiple moon charts in a
-#' single panel and does not require a square coordinate system.
+#' gggibbous: Moon charts, a pie chart alternative for two groups
+#'
+#' Moon charts are like pie charts except that the proportions are shown as
+#' crescent or gibbous portions of a circle, like the lit and unlit portions of
+#' the moon. As such, they work best with only one or two groups.
+#' \code{gggibbous} extends \code{ggplot2} to allow for plotting multiple moon
+#' charts in a single panel and does not require a square coordinate system.
 #' 
 #' The workhorse function is \code{geom_moon}, which adds a moon chart layer to
 #' a ggplot2 plot.
-#' The \code{draw_key_moon} function provides legend key glyphs for plots that
+#' The \code{draw_key_moon}, \code{draw_key_moon_left}, and
+#' \code{draw_key_full_moon} functions provides legend key glyphs for plots that
 #' use \code{geom_moon}.
-#' There are also functions for the raw \code{grid} grobs, \code{grid.moon} and
+#' There are also functions for the raw \code{grid} grobs: \code{grid.moon} and
 #' \code{moonGrob}.
+#' 
+#' For more information, see the \code{gggibbous} vignette.
 #' 
 #' @docType package
 #' @name gggibbous
