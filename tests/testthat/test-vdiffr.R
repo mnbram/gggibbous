169728573701155b128375a0e0824be550a08d1a
Mike Bramson <mnbramson@gmail.com>
2020-12-23 15:32:11 +0000

    Call vdiffr conditionally

--- tests/testthat/test-vdiffr.R
+++ tests/testthat/test-vdiffr.R
@@ -1,5 +1,12 @@
 context("test-vdiffr")
 
+# Thanks to Lionel Henry for the suggestion on using vdiffr conditionally
+# See https://github.com/lionel-/ggstance for the original
+expect_doppelganger <- function(title, fig, path = NULL, ...) {
+  testthat::skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(title, fig, path = path, ...)
+}
+
 five_moons <- data.frame(
   x = 1:5,
   y = 1,
@@ -12,16 +19,16 @@ moon_size <- ggplot(five_moons, aes(x = x, y = y, size = size)) +
   geom_moon(ratio = 0.5, right = FALSE) +
   scale_size_area(max_size = 30)
 
-vdiffr::expect_doppelganger("moons scale correctly", moon_size)
+expect_doppelganger("moons scale correctly", moon_size)
 
 moon_missing <- ggplot(
   five_moons, aes(x = x, y = y, ratio = ratio, fill = group, color = group)) +
   geom_moon(size = 10)
 
-vdiffr::expect_doppelganger("zero ratio moons are invisible", moon_missing)
+expect_doppelganger("zero ratio moons are invisible", moon_missing)
 
 full_moons <- ggplot(five_moons, aes(x = x, y = y)) +
   geom_moon(aes(ratio = ratio), fill = "black", size = 10) +
   geom_moon(aes(ratio = 1 - ratio), right = FALSE, size = 10)
 
-vdiffr::expect_doppelganger("moons can be complemented", full_moons)
+expect_doppelganger("moons can be complemented", full_moons)
